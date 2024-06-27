88e6084cb1f4c5a13c0ded145378ce5a8a01df50
smirnoffkin <vinnycontest@gmail.com>
2024-06-28 00:40:41 +0300

    complete client for blockchain

---
+++ client.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	bc "github.com/smirnoffkin/blockchain/blockchain"
+	nt "github.com/smirnoffkin/blockchain/network"
+)
+
+const (
+	ADD_BLOCK = iota + 1
+	ADD_TRNSX
+	GET_BLOCK
+	GET_LHASH
+	GET_BLNCE
+)
+
+var (
+	Addresses []string
+	User      *bc.User
+)
+
+func init() {
+	if len(os.Args) < 2 {
+		panic("failed 1")
+	}
+	var (
+		addrStr     = ""
+		userNewStr  = ""
+		userLoadStr = ""
+
+		addrExists     = false
+		userNewExists  = false
+		userLoadExists = false
+	)
+	for i := 1; i < len(os.Args); i++ {
+		arg := os.Args[i]
+		switch {
+		case strings.HasPrefix(arg, "-loadaddr:"):
+			addrStr = strings.Replace(arg, "-loadaddr:", "", 1)
+			addrExists = true
+		case strings.HasPrefix(arg, "-newuser:"):
+			userNewStr = strings.Replace(arg, "-newuser:", "", 1)
+			userNewExists = true
+		case strings.HasPrefix(arg, "-loaduser:"):
+			userLoadStr = strings.Replace(arg, "-loaduser:", "", 1)
+			userLoadExists = true
+		}
+	}
+	if !(userNewExists || userLoadExists) || !addrExists {
+		panic("failed 2")
+	}
+	err := json.Unmarshal([]byte(readFile(addrStr)), &Addresses)
+	if err != nil {
+		panic("failed 3")
+	}
+	if len(Addresses) == 0 {
+		panic("failed 4")
+	}
+	if userNewExists {
+		User = userNew(userNewStr)
+	}
+	if userLoadExists {
+		User = userLoad(userLoadStr)
+	}
+	if User == nil {
+		panic("failed 5")
+	}
+}
+
+func readFile(filename string) string {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
+
+func userNew(filename string) *bc.User {
+	user := bc.NewUser()
+	if user == nil {
+		return nil
+	}
+	err := writeFile(filename, user.Purse())
+	if err != nil {
+		return nil
+	}
+	return user
+}
+
+func userLoad(filename string) *bc.User {
+	priv := readFile(filename)
+	if priv == "" {
+		return nil
+	}
+	user := bc.LoadUser(priv)
+	if user == nil {
+		return nil
+	}
+	return user
+}
+
+func writeFile(filename, data string) error {
+	return os.WriteFile(filename, []byte(data), 0644)
+}
+
+func handleClient() {
+	var (
+		message string
+		splited []string
+	)
+	for {
+		message = inputString("> ")
+		splited = strings.Split(message, " ")
+		switch splited[0] {
+		case "/exit":
+			os.Exit(0)
+		case "/user":
+			if len(splited) < 2 {
+				fmt.Println("len(user) < 2")
+				continue
+			}
+			switch splited[1] {
+			case "address":
+				userAddress()
+			case "purse":
+				userPurse()
+			case "balance":
+				userBalance()
+			}
+		case "/chain":
+			if len(splited) < 2 {
+				fmt.Println("len(chain) < 2")
+				continue
+			}
+			switch splited[1] {
+			case "print":
+				chainPrint()
+			case "tx":
+				chainTx(splited[1:])
+			case "balance":
+				chainBalance(splited[1:])
+			}
+		default:
+			fmt.Println("undefined command\n")
+		}
+	}
+}
+
+// func main() {
+// 	handleClient()
+// }
+
+func inputString(begin string) string {
+	fmt.Print(begin)
+	msg, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	return strings.Replace(msg, "\n", "", 1)
+}
+
+func userAddress() {
+	fmt.Println("Address: ", User.Address(), "\n")
+}
+
+func userPurse() {
+	fmt.Println("Purse: ", User.Purse(), "\n")
+}
+
+func userBalance() {
+	printBalance(User.Address())
+}
+
+func chainPrint() {
+	for i := 0; ; i++ {
+		res := nt.Send(Addresses[0], &nt.Package{
+			Option: GET_BLOCK,
+			Data:   fmt.Sprintf("%d", i),
+		})
+		if res == nil || res.Data == "" {
+			break
+		}
+		fmt.Printf("[%d] => %s\n", i+1, res.Data)
+	}
+	fmt.Println()
+}
+
+func chainTx(splited []string) {
+	if len(splited) != 3 {
+		fmt.Println("len(splited) != 3")
+		return
+	}
+	num, err := strconv.Atoi(splited[2])
+	if err != nil {
+		fmt.Println("strconv error")
+		return
+	}
+	for _, addr := range Addresses {
+		res := nt.Send(addr, &nt.Package{
+			Option: GET_LHASH,
+		})
+		if res == nil {
+			continue
+		}
+		tx := bc.NewTransaction(User, bc.Base64Decode(res.Data), splited[1], uint64(num))
+		if tx == nil {
+			fmt.Println("tx is null\n")
+			return
+		}
+		res = nt.Send(addr, &nt.Package{
+			Option: ADD_TRNSX,
+			Data:   bc.SerializeTX(tx),
+		})
+		if res == nil {
+			continue
+		}
+		if res.Data == "ok" {
+			fmt.Printf("ok: (%s)\n", addr)
+		} else {
+			fmt.Println("fail: (%s)\n", addr)
+		}
+	}
+	fmt.Println()
+}
+
+func chainBalance(splited []string) {
+	if len(splited) != 2 {
+		fmt.Println("len(splited) != 2\n")
+		return
+	}
+	printBalance(splited[1])
+}
+
+func printBalance(address string) {
+	for _, addr := range Addresses {
+		res := nt.Send(addr, &nt.Package{
+			Option: GET_BLNCE,
+			Data:   address,
+		})
+		if res == nil {
+			continue
+		}
+		fmt.Printf("Balance (%s): %s coins\n", addr, res.Data)
+	}
+	fmt.Println()
+}
